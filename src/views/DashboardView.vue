45d5b6342cc4a4295635d15859de70fae9215058
Aljaž <aljaz.loncaric2@gmail.com>
2025-11-22 21:06:28 +0100

    finished /dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -8,7 +8,8 @@
 
                     <div class="today-schedule">
                         <div class="box-title"><h2>TODAY</h2></div>
-                        <div class="box-content">
+
+                        <div class="box-content" v-if="todaysLectures.length > 0">
                             <h1 class="today">{{ dayWord }}, {{ date }}</h1>
 
                             <div class="lecture-container" v-for="lecture in todaysLectures" :key="lecture.id">
@@ -31,24 +32,64 @@
                             </div>
 
                         </div>
+
+                        <div class="placeholder" v-else>
+                            <p>No Lectures Today</p>
+                        </div>
                     </div>
 
                     <div class="data-box">
-                        <div class="box-title"><h2>LAST NOTE</h2></div>
-                        <div class="box-content"></div>
+                        <div class="box-title">
+                            <h2>LAST NOTE</h2>
+                            <router-link to="/notes"><button> Manage </button></router-link>
+                        </div>
+                        <div class="box-content" v-if="notes.length > 0">
+                            <div class="content-subject">
+                                <span class="content-subject-text" :style="{backgroundColor: lastNote.color}">{{ lastNote.subjectName }}</span>
+                            </div>
+                            <h2 class="content-title">{{ lastNote.title }}</h2>
+                            <h4 class="content-date">{{ lastNote.date }} at {{ lastNote.time }}</h4>
+                            <div v-html="lastNote.content" class="content-content"></div>
+                        </div>
+                        <div class="placeholder" v-else>
+                            <p>No Notes To Show</p>
+                        </div>
                     </div>
                     <div class="data-box">
-                        <div class="box-title"><h2>NEXT ASSIGNMENT</h2></div>
-                        <div class="box-content"></div>
+                        <div class="box-title">
+                            <h2>NEXT ASSIGNMENT</h2>
+                            <router-link to="/assignments"><button> Manage </button></router-link>
+                        </div>
+                        <div class="box-content" v-if="assignments.length > 0">
+                            <div class="content-subject">
+                                <span class="content-subject-text" :style="{backgroundColor: nextAssignment.color}">{{ nextAssignment.subjectName }}</span>
+                            </div>
+                            <h2 class="content-title"> Due: {{ nextAssignment.date }}</h2>
+                            <h4 class="content-date">{{ nextAssignment.title }}</h4>
+                            <div v-html="nextAssignment.description" class="content-content"></div>
+                        </div>
+                        <div class="placeholder" v-else>
+                            <p>No Upcoming Assignments</p>
+                        </div>
                     </div>
                     <div class="data-box">
-                        <div class="box-title"><h2>NEXT TEST</h2></div>
-                        <div class="box-content"></div>
+                        <div class="box-title">
+                            <h2>NEXT TEST</h2>
+                            <router-link to="/tests"><button> Manage </button></router-link>
+                        </div>
+                        <div class="box-content" v-if="tests.length > 0">
+                            <div class="content-subject">
+                                <span class="content-subject-text" :style="{backgroundColor: nextTest.color}">{{ nextTest.subjectName }}</span>
+                            </div>
+                            <h2 class="content-title"> Due: {{ nextTest.date }}</h2>
+                            <h4 class="content-date">{{ nextTest.title }}</h4>
+                            <h4 class="content-date">{{ nextTest.type }}</h4>
+                        </div>
+                        <div class="placeholder" v-else>
+                            <p>No Upcoming Tests</p>
+                        </div>
                     </div>
-                    <div class="data-box">
-                        <div class="box-title"><h2>WEATHER</h2></div>
-                        <div class="box-content"></div>
-                    </div>
+
                 </div>
             </div>
         </main>
@@ -59,13 +100,22 @@
     import HeaderDesktop from '@/components/Elements/HeaderDesktop.vue';
     import DesktopNavbar from '@/components/Elements/DesktopNavbar.vue';
     import { getUserSchedule, getUserSubjectPositions, getUserSubjects } from '@/composables/scheduleQueries';
+    import { getUserNotes } from '@/composables/noteQueries';
+    import { getUserExams } from '@/composables/examQueries';
+    import { getUserAssignments } from '@/composables/assignmentQueries';
     import { onMounted, ref } from 'vue';
 
-    onMounted(() => {
+    const loading = ref(true)
+
+    onMounted( async () => {
         getTime()
         getToday()
         getScheduleData()
-        getTodaysPositions()
+        
+        await getTodaysPositions()
+
+        await Promise.all([loadNotes(), loadAssignments(), loadTests()])
+        loading.value = false
     })
 
     const scheduleData = ref()
@@ -115,7 +165,6 @@
         console.log(dayNumber.value)
     }
 
-    const loading = ref(true)
     const subjectPositions = ref()
     const subjects = ref()
     const todaysPositions = ref([])
@@ -169,12 +218,105 @@
 
         todaysLectures.value = todaysLectures.value.sort((a, b) => a.yPosition - b.yPosition)
 
-        loading.value = false
-
         console.log("TODAY'S LECTURES: ")
         console.log(todaysLectures.value)
     }
 
+    const notes = ref()
+    const lastNote = ref()
+    const loadNotes = async () => {
+        notes.value = await getUserNotes()
+        notes.value = notes.value.sort((a, b) => {
+            const [da, ma, ya] = a.date.split('. ').map(Number);
+            const [db, mb, yb] = b.date.split('. ').map(Number);
+
+            const [ha, mina] = a.time.split(':').map(Number);
+            const [hb, minb] = b.time.split(':').map(Number);
+
+            return new Date(ya, ma - 1, da, ha, mina) -
+                    new Date(yb, mb - 1, db, hb, minb);
+        });
+
+        console.log("NOTES: ")
+        console.log(notes.value)
+
+        lastNote.value = notes.value[notes.value.length - 1]
+        console.log(lastNote.value)
+
+        const subjectAttr = getSubjectName(lastNote.value.subjectID)
+        console.log(subjectAttr)
+
+        lastNote.value = {
+            ...lastNote.value,
+            subjectName: subjectAttr[0],
+            color: subjectAttr[1]
+        }
+        
+        console.log(lastNote.value)
+    }
+
+    const assignments = ref()
+    const nextAssignment = ref()
+    const loadAssignments = async () => {
+        assignments.value = await getUserAssignments()
+        assignments.value = assignments.value.sort((a, b) => {
+            const [da, ma, ya] = a.date.split('. ').map(Number);
+            const [db, mb, yb] = b.date.split('. ').map(Number);
+
+            return new Date(ya, ma - 1, da) - new Date(yb, mb - 1, db);
+        });
+
+        console.log("ASSIGNMENTS: ")
+        console.log(assignments.value)
+
+        nextAssignment.value = assignments.value[0]
+
+        const subjectAttr = getSubjectName(nextAssignment.value.subjectID)
+        console.log(subjectAttr)
+
+        nextAssignment.value = {
+            ...nextAssignment.value,
+            subjectName: subjectAttr[0],
+            color: subjectAttr[1]
+        }
+
+
+    }
+
+    const tests = ref()
+    const nextTest = ref()
+    const loadTests = async () => {
+        tests.value = await getUserExams()
+        tests.value = tests.value.sort((a, b) => {
+            const [da, ma, ya] = a.date.split('. ').map(Number);
+            const [db, mb, yb] = b.date.split('. ').map(Number);
+
+            return new Date(ya, ma - 1, da) - new Date(yb, mb - 1, db);
+        });
+
+        console.log("TESTS: ")
+        console.log(tests.value)
+
+        nextTest.value = tests.value[0]
+
+        const subjectAttr = getSubjectName(nextTest.value.subjectID)
+        console.log(subjectAttr)
+
+        nextTest.value = {
+            ...nextTest.value,
+            subjectName: subjectAttr[0],
+            color: subjectAttr[1]
+        }
+    }
+
+    const getSubjectName = (subjectID) => {
+        for (const subject of subjects.value) {
+            if (subject.id === subjectID) {
+                return [subject.subjectName, subject.color];
+            }
+        }
+        return null; 
+    }
 
 </script>
 
@@ -185,24 +327,52 @@
         min-height: 83vh;
         background-color: $dark;
         display: grid;
-        grid-template-rows: 49% 49%;
-        grid-template-columns: 40% 29% 29%;
+        grid-template-rows: 36% 36% 24%;
+        grid-template-columns: 49.5% 49.5%;
         gap: 1%;
         row-gap: 20px;
       }
       .today-schedule{
-        grid-row: 1 / span 2;
+        grid-row: 1 / span 3;
       }
       .today-schedule, .data-box{
         background-color: $darkest;
+        display: flex;
+        flex-direction: column;
       }
       .box-title{
         background-color: $light;
         padding: 0.8rem;
+        display: flex;
+        justify-content: space-between;
       }
       .box-content{
         padding: 0.8rem;
         color: $white;
+        .content-subject{
+            margin: 0.5rem 0;
+            .content-subject-text{
+                padding: 0.4rem;
+            }
+        }
+        .content-title{
+            font-weight: 600;
+            text-transform: capitalize;
+            margin-bottom: 0.2rem;
+        }
+        .content-date{
+            font-weight: 300;
+            margin-bottom: 0.6rem;
+        }
+      }
+      .placeholder{
+           flex: 1;                 
+           display: flex;           
+           justify-content: center; 
+           align-items: center;     
+           text-align: center; 
+           font-size: 1.4rem;
+           color: $white;
       }
       .today{
         color: $white;
